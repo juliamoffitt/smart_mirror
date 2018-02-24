953f4b7cce9a2db4cafbf9114e5746e26f290a14
Julia <juliaccmoffitt@gmail.com>
2018-02-24 13:19:30 -0800

    removed textviews and buffers, replaced with box and label system and placeholder for RHS where weather goes

--- hello.c
+++ hello.c
@@ -13,15 +13,7 @@ main (int   argc,
   gtk_init(&argc, &argv);
 
   GtkWidget *window;
-  GtkWidget *view1;
-  GtkWidget *view2;
-  GtkWidget *view3;
-  GtkWidget *view4;
   GtkWidget *button;
-
-  GtkTextBuffer *buffer;
-  GtkTextIter start, end;
-  GtkTextIter iter;
 
   GtkCssProvider *Provider = gtk_css_provider_new();
   GdkDisplay *Display = gdk_display_get_default();
@@ -30,53 +22,14 @@ main (int   argc,
   gtk_style_context_add_provider_for_screen(Screen, 
     GTK_STYLE_PROVIDER(Provider), GTK_STYLE_PROVIDER_PRIORITY_USER);
   gtk_css_provider_load_from_path(GTK_CSS_PROVIDER(Provider), 
-    "/home/julia/Coding/smart_mirror/styles.css", NULL);
+    "/home/julia/Coding/smart_mirror/smart_mirror/styles.css", NULL);
     
   // configure window
   window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
   gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
   gtk_window_set_title(GTK_WINDOW(window), "GtkTextView");
   gtk_window_fullscreen(GTK_WINDOW(window));
-
-  // view1 
-  view1 = gtk_text_view_new();
-  gtk_widget_set_name(view1, "view1");
-  buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(view1));
-  gtk_text_buffer_get_iter_at_offset(buffer, &iter, 0);
-  gtk_text_buffer_insert(buffer, &iter, get_date_string(), -1);
-  gtk_text_view_set_editable(GTK_TEXT_VIEW(view1), FALSE);
-  gtk_text_view_set_cursor_visible(GTK_TEXT_VIEW(view1), FALSE);
   
-  // view2 
-  view2 = gtk_text_view_new();
-  gtk_widget_set_name(view2, "view2");
-  buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(view2));
-  gtk_text_buffer_get_iter_at_offset(buffer, &iter, 0);
-  gtk_text_buffer_insert(buffer, &iter, local_time(), -1);
-  gtk_text_view_set_editable(GTK_TEXT_VIEW(view2), FALSE);
-  gtk_text_view_set_cursor_visible(GTK_TEXT_VIEW(view2), FALSE);
-  gtk_text_view_set_justification(GTK_TEXT_VIEW(view2), GTK_JUSTIFY_RIGHT);
-  gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(view2), GTK_WRAP_WORD);
-  
-  // view3 
-  view3 = gtk_text_view_new();
-  gtk_widget_set_name(view3, "view3");
-/*  buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(view3));
-  gtk_text_buffer_get_iter_at_offset(buffer, &iter, 0);
-  gtk_text_buffer_insert(buffer, &iter, get_time_string(), -1);
-  gtk_text_view_set_editable(GTK_TEXT_VIEW(view3), FALSE);
-  gtk_text_view_set_cursor_visible(GTK_TEXT_VIEW(view3), FALSE);
-*/
-  
-
-  // view4
-  view4 = gtk_text_view_new();
-  gtk_widget_set_name(view4, "view4");
-  buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(view4));
-  gtk_text_buffer_get_iter_at_offset(buffer, &iter, 0);
-  gtk_text_buffer_insert(buffer, &iter, local_time(), -1);
-  gtk_text_view_set_editable(GTK_TEXT_VIEW(view4), FALSE);
-  gtk_text_view_set_cursor_visible(GTK_TEXT_VIEW(view4), FALSE);
 
   button = gtk_button_new_with_label ("Quit");
   g_signal_connect_swapped (button, "clicked", 
@@ -100,25 +53,34 @@ main (int   argc,
 
   gtk_box_pack_start(GTK_BOX(h_box_1), v_box_1, TRUE, TRUE, 1);
   gtk_box_pack_start(GTK_BOX(h_box_1), v_box_2, TRUE, TRUE, 1);
-
-  gtk_box_pack_start(GTK_BOX(v_box_1), view1, FALSE, TRUE, 1);
-  //gtk_box_pack_start(GTK_BOX(v_box_1), view3, TRUE, TRUE, 1);
-  gtk_box_pack_start(GTK_BOX(v_box_2), view2, TRUE, TRUE, 1);
-
   gtk_box_pack_start(GTK_BOX(h_box_2), button, TRUE, FALSE, 1);
 
   gtk_container_add(GTK_CONTAINER(window), main_container);
   
   gchar *t;
+  gchar *d;
+  gchar *p;
+  GtkWidget *label_date;
   GtkWidget *label_time;
+  GtkWidget *label_placeholder;
+
   t = get_time_string();
   label_time = gtk_label_new(t);
   g_free (t);
   gtk_box_pack_start(GTK_BOX(v_box_1), label_time, TRUE, TRUE, 1);
-//  gtk_container_add(GTK_CONTAINER(v_box_1), label_time);
-//  gtk_text_buffer_insert(buffer, &iter, label_time, -1);
+
+  d = get_date_string();
+  label_date = gtk_label_new(d);
+  g_free (d);
+  gtk_box_pack_start(GTK_BOX(v_box_1), label_date, TRUE, TRUE, 1);
+
+  p = get_date_string();
+  label_placeholder = gtk_label_new(p);
+  g_free (p);
+  gtk_box_pack_start(GTK_BOX(v_box_2), label_placeholder, TRUE, TRUE, 1);
 
   g_timeout_add_seconds(0.5, update_label_time, label_time);
+  g_timeout_add_seconds(0.5, update_label_date, label_date);
 
   gtk_widget_show_all(window);
   gtk_main();
