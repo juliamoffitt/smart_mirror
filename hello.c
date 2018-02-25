ffb61e3ea4d2fb9d1bd7c966de19b810585280b9
Julia <juliaccmoffitt@gmail.com>
2018-02-24 17:12:27 -0800

    hooked up weather api...weird bug but program still works ???

--- hello.c
+++ hello.c
@@ -5,6 +5,12 @@
 #include <glib.h>
 #include "util.h"
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <string.h>
+
 
 int
 main (int   argc,
@@ -93,7 +99,55 @@ main (int   argc,
   g_timeout_add_seconds(0.5, update_label_time, label_time);
   g_timeout_add_seconds(0.5, update_label_date, label_date);
 
-  gtk_widget_show_all(window);
+// -----------------------------------------------------------------------------
+    struct addrinfo hints, *res;
+    int sockfd;
+
+    char buf[10000];
+    int byte_count;
+
+    //get host info, make socket and connect it
+    memset(&hints, 0,sizeof hints);
+    hints.ai_family=AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    if (getaddrinfo("api.openweathermap.org","80", &hints, &res) != 0) {
+        printf("getaddrinfo failed\n");
+    } else {
+
+        sockfd = socket(res->ai_family,res->ai_socktype,res->ai_protocol);
+        printf("Connecting...\n");
+
+        if (connect(sockfd,res->ai_addr,res->ai_addrlen) != 0){
+            printf("connect failed\n");
+        } else {
+            printf("Connected!\n");
+        }
+    
+        char *header = "GET /data/2.5/weather?q=London&appid=36f768cab0cdef430b2acf0ffbec6abb HTTP/1.1\r\nHost: api.openweathermap.org\r\n\r\n";
+        if (send(sockfd,header,strlen(header),0) != 0) {
+            printf("GET Sent...\n");
+                //all right ! now that we're connected, we can receive some data!
+            byte_count = recv(sockfd,buf,sizeof(buf)-1,0);
+            buf[byte_count] = 0;
+
+            printf("recv()'d %d bytes of data in buf\n",byte_count);
+    //        printf("%s",buf);
+            int c = 0;
+    //        while (buf[c] != '\0') {
+            //  printf("%c", buf[c]);
+              c++;
+     //       }
+            //printf("c is %d\n", c);
+    //        printf("value at c-1 is: %c\n", buf[200]);
+
+        } else {
+            printf("send failed\n");
+        }
+    }
+
+
+  //gtk_widget_show_all(window);
   gtk_main();
   
   return 0;
